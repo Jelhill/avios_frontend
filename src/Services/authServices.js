1936572a74c89476d8b69089ef942da6e93e5475
Jelhill <talk2jelili2000@gmail.com>
2021-01-17 14:49:15 +0100

    update

--- src/Services/authServices.js
+++ src/Services/authServices.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:4000/";
-// const API_URL = "https://idealbitcoin.herokuapp.com/";
+// const API_URL = "http://localhost:4000/";
+const API_URL = "https://aviosshop.herokuapp.com/";
 
 class AuthService {
   getProducts() {
